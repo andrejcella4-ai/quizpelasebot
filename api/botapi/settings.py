0ebdefa5e82bac5a1c682c8d754002c95d9c288d
grimma2 <chella05andrey@gmail.com>
2025-08-24 23:49:24 +0300

    add volumes & chnage root dir in settings

--- api/botapi/settings.py
+++ api/botapi/settings.py
@@ -107,13 +107,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = Path(os.getenv('STATIC_ROOT', BASE_DIR / 'staticfiles/'))
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = Path(os.getenv('MEDIA_ROOT', BASE_DIR / 'media/'))
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
